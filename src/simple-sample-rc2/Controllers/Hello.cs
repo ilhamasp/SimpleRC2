924fbf25a5f5eba2b755854f1734800c60267c96
Ilham Akbar Surya Permana <Ilham_P@mitrais.com>
2016-05-31 14:13:41 +0700

    Signed-off-by: Ilham Akbar Surya Permana <Ilham_P@mitrais.com>

---
+++ src/simple-sample-rc2/Controllers/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace simple_sample_rc2.Controllers
+{
+    public class Hello : Controller
+    {
+        public IConfiguration Configuration { get; }
+
+        // GET: /<controller>/
+        public string Index()
+        {
+            return $"The connection string is {Startup.ConnectionString}";
+        }
+    }
+}
